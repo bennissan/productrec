6b705080f3a3da780d557943c1b894d645a3c4fc
bennissan <ben.nissan@tufts.edu>
2018-04-03 18:54:02 -0400

    Refactored parsing code to work with DataParser class, cleaned up and consolidated functions, added documentation.

--- productrec/src/main/scala/DataParser.scala
+++ productrec/src/main/scala/DataParser.scala
@@ -1,141 +1,51 @@
-// this file will allow you to parse http://jmcauley.ucsd.edu/data/amazon/ 5-core json files
-
-import play.api.libs.json._
-import play.api.libs.json.Reads._
-import play.api.libs.functional.syntax._
+// DataParser.scala
+// Ben Nissan and Cuong Nguyen
+// Natural Language Processing
+//
+// This file uses an internal DataMap class to process a source file into into
+// a map from each term in the file to the number of times it appears in the file.
+// A term is currently defined as a single word, stripped of surrounding punctuation.
 
 import scala.io.Source
-import java.io._
+import play.api.libs.json._
 
 object DataParser {
 
-	type DataMap = collection.mutable.Map[String, Int]
-	// within Sbt's RIPL try running this line and see how the map counts every term's occurence within Musical_Instruments_5.json set of reviews
-	// dataParser.parseFromFileName("C:/_____/Desktop/productrec/productrec/src/resources/Musical_Instruments_5.json")
+	class DataMap {
+		val map = collection.mutable.Map[String, Int]()
 
-	class TermDocumentMatrix (val s : Int) {
-		val size = s
-		val matrix = collection.mutable.Map[String, Array[Int]]()
-
-		def addDocument(terms : Array[String], i : Int) = {
+		def addTerms(terms : Array[String]) = {
 			for (t <- terms) {
-				var occurrences = matrix.getOrElse(t, Array.fill[Int](size)(0))
-				occurrences(i) = 1
-				matrix.update(t, occurrences)
+				var counts = map.getOrElse(t, 0)
+				counts += 1
+				map.update(t, counts)
 			}
 		}
 
 		def getTerms() : Array[String] = {
-			matrix.keys.toArray
+			map.keys.toArray
 		}
 
-		def getOccurences : Array[Array[Int]] = {
-			matrix.values.toArray
+		def getCounts() : Array[Int] = {
+			map.values.toArray
 		}
 	}
 
-	def preprocess(sourceFile: String) : DataMap = {
-		var trainingDataMap : DataMap = collection.mutable.Map[String, Int]()
-		
+	def parse(sourceFile: String) : DataMap = {
+		val dataMap = new DataMap()
 		val source = Source.fromFile(sourceFile)
 		val reviews = source.getLines
 		val numReviews = reviews.size
 		
-		var i = 0
-
 		while (reviews.hasNext) {
 			val review = reviews.next()
 			val json = Json.parse(review)
 			val reviewText = Json.stringify((json \ "reviewText").get)
-			val terms = splitTerms(reviewText)
-			trainingDataMap = addTermsToTermsMap(terms, trainingDataMap, i, numReviews)
-			i += 1
+			val terms = reviewText.replaceAll("[.!?,;:]", "").split(" ")
+			dataMap.addTerms(terms)
 		}
 
-		return trainingDataMap
+		return dataMap
 	}
-
-	// needs improved splitting of text based on exclamations, periods, comas, etcs
-	def splitTerms(reviewText: String) : Array[String] = {
-		val termsArray = reviewText.split(" ")
-		return termsArray
-	}
-
-	// iterate through the inputTermsArray and add it to the map of termsArray
-	// if there are a total of 4 total documents, then Map should be i.e key = "dog" value = [0, 1, 0, 1] if documents 2 and 4 have the term dog
-	def addTermsToTermsMap(inputTermsArray: Array[String], inputTermsMap: DataMap,
-		                   reviewIndex: Int, totalNumberOfReviews: Int) : DataMap = {
-		
-		for(t <- inputTermsArray){
-			if (inputTermsMap.contains(t )) {
-		     }  else {
-		     	val emptyArray = 
-		     		inputTermsMap += (t ->  0 )
-		     		// inputTermsMap += (t ->  Array.fill(1)(0) )
-		           // inputTermsMap += (t ->  Array.fill(1)(0) )
-		       		// println(inputTermsMap(t)(1))
-
-		      }
-		      var mappedValue  = inputTermsMap(t)
-		     	// mappedValue(reviewIndex) = mappedValue(reviewIndex) + 1
-		     	mappedValue = mappedValue + 1
-		     	inputTermsMap(t) = mappedValue
-		     	// println(t)
-		     	// println(mappedValue)
-			}
-			return inputTermsMap
-	}
-
-	def getTermsArrayFromMap(inputTermsMap: DataMap): Array[String] = {
-		val pairs = inputTermsMap.toSeq.sortBy(_._1)
-		val (keys, vals) = pairs.unzip
-		println (keys.toArray)
-		return keys.toArray
-		
-	}
-
-	def getOccurencesArrayFromMap(inputTermsMap: DataMap): Array[Int] = {
-		val pairs = inputTermsMap.toSeq.sortBy(_._1)
-		val (keys, vals) = pairs.unzip
-		println (vals.toArray)
-		return vals.toArray
-	}
-
-	def parseJsonExampleFunction(jsonString:String) = {
-// refer to here for the example
-// https://github.com/playframework/play-json
-
-		val json: JsValue = Json.parse("""
-			{
-			  "name" : "Watership Down",
-			  "location" : {
-			    "lat" : 51.235685,
-			    "long" : -1.309197
-			  },
-			  "residents" : [ {
-			    "name" : "Fiver",
-			    "age" : 4,
-			    "role" : null
-			  }, {
-			    "name" : "Bigwig",
-			    "age" : 6,
-			    "role" : "Owsla"
-			  } ]
-			}
-			""")
-
-		// val json: JsValue = Json.parse(jsonString2)
-		val jsonString3 = Json.stringify(json)
-
-
-		val lat = (json \ "location" \ "lat").get
-		println(lat)
-
-		val bigwig = (json \ "residents" \ 1).get
-
-		println(jsonString3)
-	}
-
 
 }
-
--- productrec/src/main/scala/LSA.scala
+++ productrec/src/main/scala/LSA.scala
@@ -1,45 +1,23 @@
-// this file will use the dataParser's parseFromFileName function to get an array of terms ("termsArray"). 
-// By multiplying correlationMatrix = termsArray*transpose(termsArray), we can take the eigenvalues+eigenvectors of this correlationMatrix
-// eigenvectors will be the weighting of each individual term's contribution to a certain topic (i.e MusicalInstruments_5)
+// LSA.scala
+// Ben Nissan and Cuong Nguyen
+// Natural Language Processing
+//
+// This file uses DataParser's parse function to process a source file into a map from each term
+// in the file to the number of times it appears in the file.  From this map, it obtains an array
+// of term counts, using it to build a correlation matrix.  The eigenvalues and eigenvectors of this
+// matrix can then be used to weight each individual term's contribution to a certain topic.
 
 import breeze.linalg._
 
 object LSA {
 	
 	def run(sourceFile: String) = {
-		val parserInstance = DataParser
-		val trainingDataMap = parserInstance.preprocess(sourceFile)
-		val trainingTermsArray = parserInstance.getTermsArrayFromMap(trainingDataMap)
-		val trainingOccurencesArray = parserInstance.getOccurencesArrayFromMap(trainingDataMap)
+		val trainingDataMap = DataParser.parse(sourceFile)
+		val trainingTerms = trainingDataMap.getTerms()
+		val trainingCounts = trainingDataMap.getCounts()
 
-		val correlationMatrix = getCorrelationMatrix(DenseVector(trainingOccurencesArray:_*))
-		// println(DenseVector(trainingOccurencesArray:_*))
-		// return (DenseVector(trainingOccurencesArray:_*))
-		// val eigenvectors = getEigenvectors(correlationMatrix)
-		// val eigenvalues =
-		// getEigenvalues(correlationMatrix)
-		// val (er, ei, _) = eig(correlationMatrix)
-
-		// val eig(a)._3
+		val trainingCountsVector = DenseVector(trainingCounts:_*)
+		val correlationMatrix = trainingCountsVector * trainingCountsVector.t
 	}
-
-	def getCorrelationMatrix(trainingOccurencesArray: DenseVector[Int]) : DenseMatrix[Int] = {
-		println (trainingOccurencesArray.length)
-		val correlationMatrix = trainingOccurencesArray * trainingOccurencesArray.t
-		println(correlationMatrix)
-		return correlationMatrix
-	}
-
-
-	def getEigenvalues(correlationMatrix: DenseMatrix[Int]) = {
-		// eig(correlationMatrix)
-	}
-
-
-	def getEigenvectors(correlationMatrix: DenseMatrix[Int]) = {
-
-	}
-
 
 }
-
